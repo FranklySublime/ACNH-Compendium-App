38d6c19ca5238a5a47de86d8e380478788c91b5d
FranklySublime <francispoliquin@gmail.com>
2021-12-10 10:08:18 -0500

    signup and signin handlers done

--- client/src/components/Signin/index.js
+++ client/src/components/Signin/index.js
@@ -1,12 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signin = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	let navigate = useNavigate();
+
+	const handleSignin = (e) => {
+		e.preventDefault();
+		let user = {
+			username: username,
+			password: password,
+		};
+		fetch("/user/signin", {
+			method: "POST",
+			body: JSON.stringify(user),
+			headers: {
+				Accept: "application/json",
+				"Content-type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				console.log("JSON", json);
+				navigate("../", { replace: true });
+			});
+	};
+
 	return (
 		<div>
-			<form>
-				<input placeholder="Username" />
-				<input placeholder="Password" />
+			<form onSubmit={(e) => handleSignin(e)}>
+				<input
+					type="text"
+					placeholder="Username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 				<button>Sign-In</button>
 			</form>
 			<div>Don't have an account?</div>
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,18 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+
+	let navigate = useNavigate();
+
 	const handleSignup = (e) => {
 		e.preventDefault();
+		let user = {
+			username: username,
+			email: email,
+			password: password,
+			firstName: firstName,
+			lastName: lastName,
+		};
+		fetch("/user/signup", {
+			method: "POST",
+			body: JSON.stringify(user),
+			headers: {
+				Accept: "application/json",
+				"Content-type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				console.log("JSON", json);
+				navigate("../signin", { replace: true });
+			});
 	};
 
 	return (
 		<div>
-			<form>
-				<input placeholder="Username" />
-				<input placeholder="email" />
-				<input placeholder="Password" />
-				<input placeholder="First Name" />
-				<input placeholder="Last Name" />
+			<form onSubmit={(e) => handleSignup(e)}>
+				<input
+					type="text"
+					placeholder="Username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type="email"
+					placeholder="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<input
+					type="text"
+					placeholder="First Name"
+					value={firstName}
+					onChange={(e) => setFirstName(e.target.value)}
+				/>
+				<input
+					type="text"
+					placeholder="Last Name"
+					value={lastName}
+					onChange={(e) => setLastName(e.target.value)}
+				/>
 				<button>Create Account</button>
 			</form>
 		</div>
